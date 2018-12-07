87153c3b85982bcdf40fefb3ad879bc5e20aa823
Taka Toizumi <ttoizmi@gmail.com>
2018-12-07 23:11:12 +0900

    Initial commit from local

---
+++ sample.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+import numpy as np
+from skimage import data
+from matplotlib import pyplot as plt
+#from fast_glcm import fast_glcm_mean
+import fast_glcm
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+    img = data.camera()
+    h,w = img.shape
+    glcm_mean = fast_glcm.fast_glcm_mean(img)
+
+    plt.imshow(glcm_mean)
+    plt.tight_layout()
+    plt.show()
